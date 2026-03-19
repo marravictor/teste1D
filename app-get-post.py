986f2f5479443d93d95dde4b7f5162c2d58ccaa5
marravictor <marravictor6@gmail.com>
2026-03-19 11:35:59 -0300

    codigo push adicionado

--- app-get-post.py
+++ app-get-post.py
@@ -1,4 +1,25 @@
-print("Teste")
-print("alterando")
-print("-") * 39
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+class Servidor(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello world, Platini me da 10")
+
+   
+    def do_POST(self):
+
+        tamanho = int(self.headers['Content-Length'])
+
+        dados = self.rfile.read(tamanho)
+
+        print("Dados recebidos: ", dados.decode())
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"POST RECEBIDO")
+
+
+HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
 
